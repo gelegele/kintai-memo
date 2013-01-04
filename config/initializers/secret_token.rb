719def0a7759afeeb7ca927cfcd48e8af0791816
gelegele <ryo@ryo-macbook.local>
2013-01-04 19:23:53 +0900

    rails new kintai-memo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KintaiMemo::Application.config.secret_token = '33a1fbd6f887400eddb2b769c5f0d1fabb707426b790c885c5792c4b3eaf3c7af21473ab42ad21aa016031af8eaa2adb0302d438b6c7a8a6d37fa1b54b817ee2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+KintaiMemo::Application.config.session_store :cookie_store, key: '_kintai-memo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# KintaiMemo::Application.config.session_store :active_record_store
