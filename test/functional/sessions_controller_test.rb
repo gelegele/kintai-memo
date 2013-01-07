4c866ef41d92d7d63a5aa57217b7fe1fdb32ba69
gelegele <ryo@ryo-macbook.local>
2013-01-07 16:41:33 +0900

    rake test:functionals for sessions

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -1,14 +1,28 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
-    assert_response :success
+  def setup
+    User.create(email:'taro@h.com', password:'taro', password_confirmation:'taro')
+  end
+
+  test "should post create" do
+    post :create, {email:'taro@h.com', password:'taro', password_confirmation:'taro'}
+    assert_redirected_to time_records_url
+  end
+
+  test "should not post create without email" do
+    post :create, {password:'taro', password_confirmation:'taro'}
+    assert_redirected_to login_url
   end
 
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  test "should delete destroy" do
+    delete :destroy, {id:1}
+    assert_redirected_to login_url
   end
 
 end
