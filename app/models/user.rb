5f84fdbf05fdfed58d70e85104525a6fdf7fefa0
gelegele <Administrator@ishibashi-pc.cad.css.fujitsu.com>
2013-01-15 19:32:42 +0900

    order user's time_records by date

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :email, :password, :password_confirmation
-  has_many :time_records, :dependent => :destroy
+  has_many :time_records, :dependent => :destroy, :order => 'date'
 
   validates :email,
     :presence => true,
