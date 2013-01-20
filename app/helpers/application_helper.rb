822863f4c4c7be4da1e0667e152633a6c3ada90a
gelegele <ryo@ryo-macbook.local>
2013-01-20 09:49:28 +0900

    update strf_date, strf_time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def strf_day(time)
-    time.strftime("%m-%d %a")
+    time.strftime("%m-%d %a") if time
   end
 
   def strf_time(time)
-    time.strftime("%H:%M")
+    time.strftime("%H:%M") if time
   end
 end
