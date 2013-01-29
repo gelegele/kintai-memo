3fed07294ceeb545691972d4e2d67cfed1ef03d4
gelegele <Administrator@ishibashi-pc.cad.css.fujitsu.com>
2013-01-29 19:10:19 +0900

    wday style in time_records index

--- app/helpers/time_records_helper.rb
+++ app/helpers/time_records_helper.rb
@@ -1,2 +1,11 @@
 module TimeRecordsHelper
+  def wday_class(date)
+    if date.wday == 0
+      'tr-holiday'
+    elsif date.wday == 6
+      "tr-saturday"
+    else
+      "tr-weekday"
+    end
+  end
 end
