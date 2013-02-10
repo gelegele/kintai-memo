d6f7f027d1430c7736a7202c398f3875e1fd8912
gelegele <ryo@softbank126102029231.bbtec.net>
2013-02-11 07:54:54 +0900

    use ENV[SECRET_TOKEN] as secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-KintaiMemo::Application.config.secret_token = '33a1fbd6f887400eddb2b769c5f0d1fabb707426b790c885c5792c4b3eaf3c7af21473ab42ad21aa016031af8eaa2adb0302d438b6c7a8a6d37fa1b54b817ee2'
+KintaiMemo::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'fallback_token_for_development'
