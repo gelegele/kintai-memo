ce518b85ca97a6aa586f504ea58d919f6ee6012b
gelegele <ryo@softbank126102029238.bbtec.net>
2013-03-03 09:53:56 +0900

    fix a bug. remove has_many working_time in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :admin
   has_many :monthlies, :dependent => :destroy, :order => 'year desc, month desc'
   has_many :time_tables, :dependent => :destroy
-  has_many :working_times, :dependent => :destroy, :order => 'start_hours'
 
   validates :email,
     :presence => true,
